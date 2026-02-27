f06adc01c583cdb3cfeb2338c17cb21b64e212a1
Anthony <schneppanthony@gmail.com>
2026-02-27 16:03:09 +0100

    Send data using MQTT

--- include/env.h
+++ include/env.h
@@ -1,1 +1,15 @@
 #define SENDER_CODE false
+
+// #define WIFI_SSID "CESI_Iot"
+// #define WIFI_PASSWORD "#RO_i0t.n3t"
+
+#define WIFI_SSID "Femboy_lover"
+#define WIFI_PASSWORD "Zobito67"
+
+// #define LOCAL_IP IPAddress(169, 254, 217, 55)
+// #define GATEWAY IPAddress(169, 254, 217, 127)
+// #define SUBNET IPAddress(255, 255, 0, 0)
+// #define DNS IPAddress(169, 254, 217, 127)
+
+#define MQTT_SERVER_IP "10.78.154.106"
+#define MQTT_SERVER_PORT 1883
--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,16 @@
 #else
     #include <receiver.h>
     #include <RTCManager.h>
+    #include <Wifi.h>
+    #include <PubSubClient.h>
 #endif
 
 #if SENDER_CODE
 #else
     RTCManager rtc;
     Receiver receiver;
+    WiFiClient wifiClient;
+    PubSubClient pubSubClient(wifiClient);
 #endif
 
 void setup()
@@ -19,17 +23,32 @@ void setup()
         senderSetup();
     #else
         Serial.begin(9600);
-        while (!Serial); // wait for serial port to connect (native USB)
+        while (!Serial)
+            ; // wait for serial port to connect (native USB)
         Wire.begin();
-        if (!rtc.begin()) {
+        if (!rtc.begin())
+        {
             Serial.println("Couldn't find RTC");
             Serial.flush();
         }
 
-        if (!rtc.isRunning()) {
+        if (!rtc.isRunning())
+        {
             Serial.println("RTC was not running - time set to compile time.");
         }
         receiver.setup();
+
+        // Wifi setup
+        // WiFi.config(LOCAL_IP, GATEWAY, SUBNET, DNS);
+        WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+        while (WiFi.status() != WL_CONNECTED)
+        {
+            delay(500);
+            Serial.print(".");
+        }
+
+        pubSubClient.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
     #endif
 }
 
@@ -38,7 +57,34 @@ void loop()
     #if SENDER_CODE
         senderLoop();
     #else
-        String data;
-        data = receiver.ReceiveData();
+        if (!pubSubClient.connected())
+        {
+            while (!pubSubClient.connected())
+            {
+                Serial.print("Connecting to MQTT...");
+
+                if (pubSubClient.connect("ESP32Client"))
+                {
+                    Serial.println("connected");
+                }
+                else
+                {
+                    Serial.print("failed, rc=");
+                    Serial.print(pubSubClient.state());
+                    delay(2000);
+                }
+            }
+        }
+
+        pubSubClient.loop();
+
+        String jsonData;
+        jsonData = receiver.ReceiveData();
+
+        if (jsonData.length() > 0)
+        {
+            jsonData += R"(","timestamp":")" + rtc.getISO() + R"("})";
+            pubSubClient.publish("Edusign", jsonData.c_str());
+        }
     #endif
 }
